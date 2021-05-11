53768e9e166180d2e791f2ec1affb3c1496f5bfc
Wallace <wallace_wolv@hotmail.com>
2021-05-11 18:44:45 -0300

    Integrating stripe and server side render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  
   return (
     <>
       <Head>
@@ -17,13 +27,33 @@ export default function Home() {
           <h1>New about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton  priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ipjr5KfIsobNQtCzyHG3Zl7', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-Us', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+  
+  return {
+    props: {
+      product,
+    }
+  }
 }
